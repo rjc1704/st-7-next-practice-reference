42d79117ff486ef6b4f6b10fface5b42485ae5a6
Jecheon Ryu <rjc1704@gmail.com>
2024-12-16 11:15:02 +0900

    일부 수정

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -6,19 +6,22 @@ import {
   QueryClientProvider,
 } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useState } from "react";
 type Props = {
   children: React.ReactNode;
 };
 
-const queryClient = new QueryClient({
-  queryCache: new QueryCache({
-    onError: (error, query) => {
-      alert(`Error Occurred in ${query.queryKey}: ${error.message}`);
-    },
-  }),
-});
-
 export default function Providers({ children }: Props) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        queryCache: new QueryCache({
+          onError: (error, query) => {
+            alert(`Error Occurred in ${query.queryKey}: ${error.message}`);
+          },
+        }),
+      }),
+  );
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
